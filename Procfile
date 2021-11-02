7ffff1b04d41da56bc0f8c9dc357e104b77f6409
Hiimus <hiimhanseneirik@gmail.com>
2021-11-02 12:02:34 +0000

    056 - deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ci_boutique.wsgi:application
--- ci_boutique_v1/settings.py
+++ ci_boutique_v1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-im%7g8*#c@h#zxb8f9na)=9fk5=o469w34!6z(h0=3j)y()w=-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hiimus-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'ci_boutique_v1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
