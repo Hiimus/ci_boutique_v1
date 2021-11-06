fa987febc2032e2eb832fa566e593fd32f5c833e
Hiimus <hiimhanseneirik@gmail.com>
2021-11-06 16:05:22 +0000

    061 sending real emails, added smtp to EMAIL_BACKEND in settings.

--- ci_boutique_v1/settings.py
+++ ci_boutique_v1/settings.py
@@ -213,7 +213,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 578
     EMAIL_HOST = 'smtp.gmail.com'
