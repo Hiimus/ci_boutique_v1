dbd1df3a17b7929efb6256f124d91710a90a3126
Hiimus <hiimhanseneirik@gmail.com>
2021-11-07 17:23:43 +0000

    Fixed errors and warnings

--- bag/contexts.py
+++ bag/contexts.py
@@ -19,7 +19,7 @@ def bag_contents(request):
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product': product, 
+                'product': product,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
@@ -41,7 +41,7 @@ def bag_contents(request):
         free_delivery_delta = 0
 
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
